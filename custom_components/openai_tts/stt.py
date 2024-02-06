e1cae7e32392d587b82d4162a3194ac1938c4a62
lurebat <lurebatter@gmail.com>
2024-02-06 21:30:28 +0200

    win

--- custom_components/openai_tts/stt.py
+++ custom_components/openai_tts/stt.py
@@ -2,7 +2,8 @@
 Support for OpenAI STT.
 """
 import logging
-import requests
+from typing import AsyncIterable
+import httpx
 import async_timeout
 import voluptuous as vol
 from homeassistant.components.tts import CONF_LANG
@@ -19,6 +20,8 @@ from homeassistant.components.stt import (
 )
 from homeassistant.core import HomeAssistant
 import homeassistant.helpers.config_validation as cv
+import wave
+import io
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -26,12 +29,17 @@ CONF_API_KEY = 'api_key'
 DEFAULT_LANG = 'en-US'
 OPENAI_STT_URL = "https://api.openai.com/v1/audio/transcriptions"
 CONF_MODEL = 'model'
+CONF_URL = 'url'
+CONF_PROMPT = 'prompt'
+CONF_TEMPERATURE = 'temperature'
 
 PLATFORM_SCHEMA = cv.PLATFORM_SCHEMA.extend({
     vol.Required(CONF_API_KEY): cv.string,
     vol.Optional(CONF_LANG, default=DEFAULT_LANG): cv.string,
-    vol.Optional(CONF_MODEL, default='whisper-1'): cv.string
-    vol.Optional('url', default=None): cv.string
+    vol.Optional(CONF_MODEL, default='whisper-1'): cv.string,
+    vol.Optional(CONF_URL, default=None): cv.string,
+    vol.Optional(CONF_PROMPT, default=None): cv.string,
+    vol.Optional(CONF_TEMPERATURE, default=0): cv.positive_int,
 })
 
 async def async_get_engine(hass, config, discovery_info=None):
@@ -40,29 +48,33 @@ async def async_get_engine(hass, config, discovery_info=None):
     language = config.get(CONF_LANG, DEFAULT_LANG)
     model = config.get(CONF_MODEL)
     url = config.get('url')
-    return OpenAISTTProvider(hass, api_key, language, model, url)
+    prompt = config.get('prompt')
+    temperature = config.get('temperature')
+    return OpenAISTTProvider(hass, api_key, language, model, url, prompt, temperature)
 
 class OpenAISTTProvider(Provider):
     """The OpenAI STT API provider."""
 
-    def __init__(self, hass, api_key, lang, model, url):
+    def __init__(self, hass, api_key, lang, model, url, prompt, temperature):
         """Initialize OpenAI STT provider."""
         self.hass = hass
         self._api_key = api_key
         self._language = lang
         self._model = model
         self._url = url
+        self._prompt = prompt
+        self._temperature = temperature
 
     @property
     def default_language(self) -> str:
         """Return the default language."""
-        return self._language
+        return self._language.split(',')[0]
 
     @property
     def supported_languages(self) -> list[str]:
         """Return the list of supported languages."""
         # Ideally, this list should be dynamically fetched from OpenAI, if supported.
-        return [self._language]
+        return self._language.split(',')
 
     @property
     def supported_formats(self) -> list[AudioFormats]:
@@ -89,14 +101,25 @@ class OpenAISTTProvider(Provider):
         """Return a list of supported channels."""
         return [AudioChannels.CHANNEL_MONO]
 
-
     async def async_process_audio_stream(
-        self, metadata: SpeechMetadata, stream
+        self, metadata: SpeechMetadata, stream: AsyncIterable[bytes]
     ) -> SpeechResult:
-        # Collect data
-        audio_data = b""
-        async for chunk in stream:
-            audio_data += chunk
+        if metadata.format != AudioFormats.WAV:
+            _LOGGER.error("Unsupported audio format: %s", metadata.format)
+            return SpeechResult("", SpeechResultState.ERROR)
+
+        wav_stream = io.BytesIO()
+
+        with wave.open(wav_stream, 'w') as wav_file:
+            wav_file.setnchannels(metadata.channel)
+            wav_file.setsampwidth(2)
+            wav_file.setframerate(metadata.sample_rate)
+            
+            async for chunk in stream:
+                wav_file.writeframes(chunk)
+
+        wav =  wav_stream.getvalue() # wav file object 
+
 
         # OpenAI API endpoint for audio transcription
         url = self._url or OPENAI_STT_URL
@@ -107,29 +130,22 @@ class OpenAISTTProvider(Provider):
         }
         # Prepare the data for the POST request
         files = {
-            'file': ('filename.wav', audio_data, 'audio/wav')
+            'file': ('filename.wav', wav, 'audio/wav')
         }
         data = {
             'model': self._model,
             'language': self._language,
-            # 'prompt': 'Your prompt here',  # Optional
-            # 'response_format': 'json',  # Optional, defaults to json
-            # 'temperature': 0  # Optional
+            'temperature': self._temperature,
+            'prompt': self._prompt
         }
-        def job():
-            response = requests.post(url, headers=headers, files=files, data=data)
-            if response.status_code == 200:
-                return response.text
-            else:
-                _LOGGER.error("%s", response.text)
-                return ''
-
         async with async_timeout.timeout(10):
-            assert self.hass
-            response = await self.hass.async_add_executor_job(job)
-            if len(response) > 0:
-                return SpeechResult(
-                    response.text,
-                    SpeechResultState.SUCCESS,
-                )
-            return SpeechResult("", SpeechResultState.ERROR)
+            async with httpx.AsyncClient() as client:
+                response = await client.post(url, headers=headers, files=files, data=data)
+                if response.status_code == 200:
+                    return SpeechResult(
+                        response.text,
+                        SpeechResultState.SUCCESS,
+                    )
+                else:
+                    _LOGGER.error("%s", response.text)
+                    return SpeechResult("", SpeechResultState.ERROR)
